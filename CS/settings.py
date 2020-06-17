a01f54a5e9ebf7541b66f1d8db06c5595ae0bbfd
gabriela denisse hernandez penagos <183422@ids.upchiapas.edu.mx>
2020-06-17 02:04:30 -0500

    change

--- CS/settings.py
+++ CS/settings.py
@@ -44,6 +44,13 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':('rest_framework.permissions.IsAuthenticated',),
+    'DEFAULT_AUTHENTICATION_CLASSES':('rest_framework.authentication.TokenAuthentication',),
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE':100
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
