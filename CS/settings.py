badd40d9fdf71b9549a2c3db3eba151ec3626b81
gabriela denisse hernandez penagos <183422@ids.upchiapas.edu.mx>
2020-07-03 00:28:51 -0500

    change3

--- CS/settings.py
+++ CS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['clienteservidor5-1.herokuapp.com']
+ALLOWED_HOSTS = ['clienteservidor5-2.herokuapp.com']
 
 
 # Application definition
