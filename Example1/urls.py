a3f95f5b1cf4407d3ced4df8e75add322a2723f1
unknown <183422@ids.upchiapas.edu.mx>
2020-08-02 22:48:22 -0500

    add new url

--- Example1/urls.py
+++ Example1/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls import include, url
 from Example1 import views
 
 urlpatterns = [
-    re_path(r'example1/$', views.ExampleList.as_view()),
-    re_path(r'example1_detail/(?P<id>\d+)/$', views.ExampleDetail.as_view()),
+    re_path(r'example1', views.ExampleList.as_view()),
+    re_path(r'example1_detail/(?P<id>\d+)', views.ExampleDetail.as_view()),
 ]
